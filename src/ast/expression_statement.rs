44755f0dc2e6d74066ee67385649b22d3ffb66c3
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-01-21 21:14:03 +0100

    added test for expression statement

--- src/ast/expression_statement.rs
+++ src/ast/expression_statement.rs
@@ -17,3 +17,41 @@ impl AstNode for ExpressionStatement {
         self.expression.string()
     }
 }
+
+#[cfg(test)]
+mod test {
+    use core::panic;
+
+    use crate::{
+        ast::{ExpressionNode, StatementNode},
+        parser::Parser,
+    };
+
+    #[test]
+    fn test_expression_statement() {
+        let input = "foobar;";
+        let mut parser = Parser::new(input.into());
+
+        let (program, errors) = parser.parse_program();
+        let empty: Vec<String> = vec![];
+
+        assert_eq!(errors, empty);
+        assert_eq!(program.statements.len(), 1);
+
+        let mut nodes = program.statements.into_iter();
+        let node = nodes.next().unwrap();
+
+        let StatementNode::ExpressionStatement(expression) = node else {
+            panic!("expected ExpressionStatement for node, got {:?}", node);
+        };
+
+        let ExpressionNode::Identifier(iden) = expression.expression else {
+            panic!(
+                "expected Identifier for expression, got {:?}",
+                expression.expression
+            );
+        };
+
+        assert_eq!(iden.value, "foobar");
+    }
+}
