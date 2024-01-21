9419fc08531b403d984d666e88568265059c72c7
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-01-21 10:57:33 +0100

    added token enum

---
+++ src/tokens/token.rs
@@ -0,0 +1,21 @@
+pub enum Token {
+    ILLEGAL,
+    EOF,
+
+    IDENT(String),
+    INT(u64),
+
+    ASSIGN,
+    PLUS,
+
+    COMMA,
+    SEMICOLON,
+
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+
+    FUNCTION,
+    LET,
+}
