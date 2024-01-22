0992afd8a5c38d23f8cdbe75343e7024fcfb539d
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-01-22 23:15:10 +0100

    added token->precedence conversion

--- src/parser/precedence.rs
+++ src/parser/precedence.rs
@@ -1,3 +1,5 @@
+use crate::tokens::token::Token;
+
 #[derive(PartialEq, Eq, PartialOrd, Ord)]
 pub enum Precedence {
     LOWEST,
@@ -7,6 +9,32 @@ pub enum Precedence {
     PRODUCT,
     PREFIX,
     CALL,
+}
+
+impl From<&Token> for Precedence {
+    fn from(value: &Token) -> Self {
+        match value {
+            Token::EQ => Precedence::EQUALS,
+            Token::NOT_EQ => Precedence::EQUALS,
+            Token::LT => Precedence::LESSGREATER,
+            Token::GT => Precedence::LESSGREATER,
+            Token::PLUS => Precedence::SUM,
+            Token::MINUS => Precedence::SUM,
+            Token::SLASH => Precedence::PRODUCT,
+            Token::ASTERISK => Precedence::PRODUCT,
+            _ => Precedence::LOWEST,
+        }
+    }
+}
+
+pub trait IntoPrecedence {
+    fn precedence(&self) -> Precedence;
+}
+
+impl IntoPrecedence for Token {
+    fn precedence(&self) -> Precedence {
+        Precedence::from(self)
+    }
 }
 
 #[cfg(test)]
