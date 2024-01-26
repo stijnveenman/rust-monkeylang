d5703da363fb4bf2fcf6571cfda78b39347021d8
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-01-26 23:22:59 +0100

    added test_if_else_statement

--- src/ast/if_expression.rs
+++ src/ast/if_expression.rs
@@ -72,4 +72,45 @@ mod test {
 
         test_expression(&expression.expression, &"x");
     }
+
+    #[test]
+    fn test_if_else_statement() {
+        let input = "if (x < y) { x } else { y }";
+        let mut parser = Parser::new(input.into());
+
+        let (program, errors) = parser.parse_program();
+        let empty: Vec<String> = vec![];
+
+        assert_eq!(errors, empty);
+        assert_eq!(program.statements.len(), 1);
+
+        let node = program.statements.first().unwrap();
+        let StatementNode::ExpressionStatement(expression) = node else {
+            panic!("expected ExpressionStatement for node, got {:?}", node);
+        };
+
+        let ExpressionNode::IfExpression(if_expression) = &expression.expression else {
+            panic!("expected IfExpression for node, got {:?}", node);
+        };
+
+        test_infix_expression(&if_expression.condition, "x", Token::LT, "y");
+
+        assert_eq!(if_expression.concequence.statements.len(), 1);
+        let node = if_expression.concequence.statements.first().unwrap();
+        let StatementNode::ExpressionStatement(expression) = node else {
+            panic!("expected ExpressionStatement for node, got {:?}", node);
+        };
+
+        test_expression(&expression.expression, &"x");
+
+        let alternative = &if_expression.alternative.as_ref().unwrap();
+
+        assert_eq!(alternative.statements.len(), 1);
+        let node = alternative.statements.first().unwrap();
+        let StatementNode::ExpressionStatement(expression) = node else {
+            panic!("expected ExpressionStatement for node, got {:?}", node);
+        };
+
+        test_expression(&expression.expression, &"y");
+    }
 }
