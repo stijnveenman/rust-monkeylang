3a4089703b017f4f4d8c37e699d4b96472b52c6e
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-01-27 10:58:46 +0100

    added function expression tests

--- src/ast/function_expression.rs
+++ src/ast/function_expression.rs
@@ -26,3 +26,48 @@ impl AstNode for FunctionExpression {
         )
     }
 }
+
+#[cfg(test)]
+mod test {
+
+    use crate::{
+        ast::{infix_expression::test::test_infix_expression, ExpressionNode, StatementNode},
+        parser::Parser,
+        tokens::token::Token,
+    };
+
+    #[test]
+    fn test_if_statement() {
+        let input = "fn(x, y) { x + y }";
+        let mut parser = Parser::new(input.into());
+
+        let (program, errors) = parser.parse_program();
+        let empty: Vec<String> = vec![];
+
+        assert_eq!(errors, empty);
+        assert_eq!(program.statements.len(), 1);
+
+        let node = program.statements.first().unwrap();
+        let StatementNode::ExpressionStatement(expression) = node else {
+            panic!("expected ExpressionStatement for node, got {:?}", node);
+        };
+
+        let ExpressionNode::FunctionExpression(fn_expression) = &expression.expression else {
+            panic!("expected FunctionExpression for node, got {:?}", node);
+        };
+
+        assert_eq!(fn_expression.parameters.len(), 2);
+
+        assert_eq!(&fn_expression.parameters.first().unwrap().value, &"x");
+        assert_eq!(&fn_expression.parameters.get(1).unwrap().value, &"y");
+
+        assert_eq!(fn_expression.body.statements.len(), 1);
+
+        let node = fn_expression.body.statements.first().unwrap();
+        let StatementNode::ExpressionStatement(expression) = node else {
+            panic!("expected ExpressionStatement for node, got {:?}", node);
+        };
+
+        test_infix_expression(&expression.expression, "x", Token::PLUS, "y");
+    }
+}
