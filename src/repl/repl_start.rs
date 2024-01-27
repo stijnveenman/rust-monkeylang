ea92de565c1f97202c8ff68f9ecf7287ce666260
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-01-27 17:04:37 +0100

    updated repl to print AST

--- src/repl/repl_start.rs
+++ src/repl/repl_start.rs
@@ -1,6 +1,10 @@
 use std::io::{self, stdout, BufRead, Write};
 
-use crate::tokens::{lexer::Lexer, token::Token};
+use crate::{
+    ast::AstNode,
+    parser::Parser,
+    tokens::{lexer::Lexer, token::Token},
+};
 
 const PROMPT: &str = ">>";
 
@@ -13,15 +17,16 @@ pub fn start() {
     for line in stdin.lock().lines() {
         let line = line.expect("failed to read line from stdin");
 
-        let mut lexer = Lexer::new(line);
+        let mut parser = Parser::new(line);
 
-        loop {
-            let token = lexer.next_token();
-            println!("{:?}", token);
-            if token == Token::EOF {
-                break;
-            }
+        let (program, errors) = parser.parse_program();
+
+        if !errors.is_empty() {
+            println!("{}", errors.join("\n"));
+        } else {
+            println!("{}", program.string());
         }
+
         print!("{}", PROMPT);
         stdout().flush().expect("failed to flush stdout");
     }
