e9ab04a2304cbe3f6b41202f124237541daa4c8d
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-01-27 16:20:21 +0100

    setup 2nd unit test for call expression

--- src/ast/call_expression.rs
+++ src/ast/call_expression.rs
@@ -28,10 +28,13 @@ impl AstNode for CallExpression {
 
 #[cfg(test)]
 mod test {
+
+    use rstest::rstest;
+
     use crate::{
         ast::{
-            infix_expression::test::test_infix_expression, test::test_expression, ExpressionNode,
-            StatementNode,
+            infix_expression::test::test_infix_expression, test::test_expression, AstNode,
+            ExpressionNode, StatementNode,
         },
         parser::Parser,
         tokens::token::Token,
@@ -71,6 +74,40 @@ mod test {
             4u64,
             Token::PLUS,
             5u64,
+        );
+    }
+
+    #[rstest]
+    #[case("add();", "add", vec![])]
+    #[case("add(1);", "add", vec!["1"])]
+    #[case("add(1, 2 * 3, 4 + 5);", "add", vec!["1", "(2 * 3)", "(4 + 5)"])]
+    fn tesst_call_arguments(#[case] input: &str, #[case] name: &str, #[case] arguments: Vec<&str>) {
+        let mut parser = Parser::new(input.into());
+
+        let (program, errors) = parser.parse_program();
+        let empty: Vec<String> = vec![];
+
+        assert_eq!(errors, empty);
+        assert_eq!(program.statements.len(), 1);
+
+        let node = program.statements.first().unwrap();
+        let StatementNode::ExpressionStatement(expression) = node else {
+            panic!("expected ExpressionStatement for node, got {:?}", node);
+        };
+
+        let ExpressionNode::CallExpression(expression) = &expression.expression else {
+            panic!("expected CallExpression for node, got {:?}", node);
+        };
+
+        test_expression(&expression.function, &name.to_string());
+
+        assert_eq!(
+            expression
+                .arguments
+                .iter()
+                .map(|a| a.string())
+                .collect::<Vec<_>>(),
+            arguments
         );
     }
 }
