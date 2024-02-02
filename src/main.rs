84e21ab0d233872176d49f829d4a46be94698970
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-02-02 20:58:44 +0100

    allow file input to run

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
-use rust_monkeylang::repl;
+use std::{env, fs, process::exit};
+
+use rust_monkeylang::{
+    evaluator::{environment::Environment, eval},
+    parser::Parser,
+    repl,
+};
+
+fn run(file: &str) {
+    let content = fs::read_to_string(file).unwrap();
+
+    let mut parser = Parser::new(content);
+
+    let (program, errors) = parser.parse_program();
+
+    if !errors.is_empty() {
+        println!("ERRORS: {:?}", errors);
+        exit(1);
+    }
+
+    let result = eval(&Environment::new(), (&program).into());
+    println!("{}", result);
+}
 
 fn main() {
-    repl::start();
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        run(args.get(1).unwrap())
+    } else {
+        repl::start();
+    }
 }
