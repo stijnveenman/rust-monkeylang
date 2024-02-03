8165b1a53ff48ae6151023b57aca21bae5b6716b
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-02-03 16:06:40 +0100

    added hashable index support

--- src/evaluator/mod.rs
+++ src/evaluator/mod.rs
@@ -155,6 +155,14 @@ fn eval_hash_literal(env: &Rc<Mutex<Environment>>, expression: &HashLiteral) -> 
     Object::Hash(hm)
 }
 
+fn eval_index_hash(hm: HashMap<Object, Object>, right: Object) -> Object {
+    if !right.hashable() {
+        return Object::Error(format!("unusable as hash key: {}", right.type_str()));
+    }
+
+    hm.get(&right).unwrap_or(&Object::Null).to_owned()
+}
+
 fn eval_index(left: Object, right: Object) -> Object {
     match (left, right) {
         (Object::Array(array), Object::Integer(index)) => {
@@ -163,6 +171,7 @@ fn eval_index(left: Object, right: Object) -> Object {
             };
             array.into_iter().nth(index).unwrap_or(Object::Null)
         }
+        (Object::Hash(hash), right) => eval_index_hash(hash, right),
         (Object::Array(_), right) => Object::Error(format!(
             "index operator not supported with: {}",
             right.type_str()
