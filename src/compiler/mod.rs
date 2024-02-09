4627787fec96cc6404ea5d6088eabe8c79dca22c
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-02-09 16:39:09 +0100

    implemented basic compiler

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,4 +1,8 @@
-use crate::{ast::Node, code::Instructions, object::Object};
+use crate::{
+    ast::{ExpressionNode, Node, StatementNode},
+    code::{make::make, Instructions, Opcode},
+    object::Object,
+};
 
 pub struct Compiler {
     instructions: Instructions,
@@ -10,6 +14,8 @@ pub struct Bytecode {
     constants: Vec<Object>,
 }
 
+type R = Result<(), &'static str>;
+
 impl Compiler {
     pub fn new() -> Compiler {
         Compiler {
@@ -18,8 +24,73 @@ impl Compiler {
         }
     }
 
-    pub fn compile(&mut self, node: Node) -> Result<(), &'static str> {
+    pub fn compile(&mut self, node: Node) -> R {
+        match node {
+            Node::Statement(_) => todo!(),
+            Node::Expression(_) => todo!(),
+            Node::Program(node) => self.compile_statements(&node.statements),
+        }
+    }
+
+    fn compile_statements(&mut self, statements: &[StatementNode]) -> R {
+        for statement in statements {
+            self.compile_statement(statement)?;
+        }
+
         Ok(())
+    }
+
+    fn compile_statement(&mut self, statement: &StatementNode) -> R {
+        match statement {
+            StatementNode::LetStatement(_) => todo!(),
+            StatementNode::ReturnStatement(_) => todo!(),
+            StatementNode::BlockStatement(_) => todo!(),
+            StatementNode::ExpressionStatement(node) => self.compile_expression(&node.expression),
+        }
+    }
+
+    fn compile_expression(&mut self, expression: &ExpressionNode) -> R {
+        match expression {
+            ExpressionNode::Identifier(_) => todo!(),
+            ExpressionNode::IntegerLiteral(i) => {
+                let integer = Object::Integer(i.value);
+                let pos = self.add_constant(integer);
+
+                self.emit(Opcode::OpConstant, vec![pos]);
+
+                Ok(())
+            }
+            ExpressionNode::BooleanLiteral(_) => todo!(),
+            ExpressionNode::StringLiteral(_) => todo!(),
+            ExpressionNode::ArrayLiteral(_) => todo!(),
+            ExpressionNode::PrefixExpression(_) => todo!(),
+            ExpressionNode::InfixExpression(node) => {
+                self.compile_expression(&node.left)?;
+                self.compile_expression(&node.right)
+            }
+            ExpressionNode::IfExpression(_) => todo!(),
+            ExpressionNode::FunctionExpression(_) => todo!(),
+            ExpressionNode::CallExpression(_) => todo!(),
+            ExpressionNode::IndexExpresssion(_) => todo!(),
+            ExpressionNode::HashLiteral(_) => todo!(),
+        }
+    }
+
+    fn emit(&mut self, op: Opcode, operands: Vec<usize>) -> usize {
+        let instruction = make(op, &operands);
+        self.add_instruction(instruction)
+    }
+
+    fn add_instruction(&mut self, mut instruction: Vec<u8>) -> usize {
+        let pos = self.instructions.0.len();
+        self.instructions.0.append(&mut instruction);
+        pos
+    }
+
+    fn add_constant(&mut self, object: Object) -> usize {
+        self.constants.push(object);
+
+        self.constants.len() - 1
     }
 
     pub fn bytecode(&self) -> Bytecode {
@@ -82,9 +153,9 @@ pub mod test {
             .collect::<Vec<u8>>()
             .into();
 
-        assert_eq!(bytecode.instructions, expected_instructions,);
+        assert_eq!(bytecode.instructions, expected_instructions);
 
-        assert_eq!(bytecode.instructions, expected_instructions,);
+        assert_eq!(expected_constants.len(), bytecode.constants.len());
 
         for (expected, result) in expected_constants.iter().zip(bytecode.constants) {
             test_object(&result, expected)
