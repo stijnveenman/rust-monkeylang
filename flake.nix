63fcffb6d558a42d560d5b6ede214f66f88d1598
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-03-03 08:20:59 +0100

    add rust overlay

--- flake.nix
+++ flake.nix
@@ -2,15 +2,19 @@
   description = "A basic flake with a shell";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [rust-overlay.overlays.default];
+        };
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = [ pkgs.bashInteractive ];
+          packages = [ pkgs.rust-bin.stable.latest.default ];
         };
       });
 }
