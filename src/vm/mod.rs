ecb871890e927674508bb6d1c664b99115df1dc4
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-03-17 20:24:00 +0100

    added more closure unit tests

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -868,6 +868,47 @@ adder(8);
 ",
         11
     )]
+    #[case(
+        "
+let newAdderOuter = fn(a, b) {
+    let c = a + b;
+    fn(d) {
+        let e = d + c;
+        fn(f) { e + f; };
+    };
+};
+let newAdderInner = newAdderOuter(1, 2)
+let adder = newAdderInner(3);
+adder(8);
+",
+        14
+    )]
+    #[case(
+        "
+let a = 1;
+let newAdderOuter = fn(b) {
+    fn(c) {
+        fn(d) { a + b + c + d };
+    }; 
+};
+let newAdderInner = newAdderOuter(2)
+let adder = newAdderInner(3);
+adder(8);
+",
+        14
+    )]
+    #[case(
+        "
+let newClosure = fn(a, b) {
+    let one = fn() { a; };
+    let two = fn() { b; };
+    fn() { one() + two(); };
+};
+let closure = newClosure(9, 90);
+closure();
+",
+        99
+    )]
     fn test_closure(#[case] input: &str, #[case] expected: i64) {
         let element = test_vm(input);
         test_object(&element, &expected);
